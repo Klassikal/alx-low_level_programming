ec8e7c390c4009591fdf7e78afc1157fb7333c65
klassikal <alfredcoffie0@gmail.com>
2022-07-02 07:56:48 -0700

    j

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,26 +1,10 @@
-#!/bin/bash
 #include <stdio.h>
-
-
-
 /**
- *
- *  * main - Entry point
- *
- *   * Description: prints "\"Programming is like building a multilingual puzzle"
- *
- *    * Return: 0
- *
- *     */
-
-
-
+ * main - Entry point
+ * Return: Always 0 (success)
+ */
 int main(void)
-
 {
-
-		puts("\"Programming is like building a multilingual puzzle");
-
-			return (0);
-
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
