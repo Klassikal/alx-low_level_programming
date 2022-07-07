87fa505ff6ba2f3a3024d2c2820fbd8854002341
Oppong200 <oppongnathaniel14@gmail.com>
2022-07-07 16:07:29 +0100

    l

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -4,16 +4,13 @@
 /**
  * _isupper - check if a character is uppercase
  * @c: variable whose character is to be checked
- *
- * Created by: Sanctus-Peter
- * cc: 7th july, 2022
- *
  * Return: 1 if is uppercase, 0 if otherwise
  */
 
 int _isupper(int c)
 {
-	if (isupper(c))
+	if (isupper(C))
 		return (1);
 	return (0);
 }
+
