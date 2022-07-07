2daebf9c3c51f5cf8d475107fee6c43f360336df
klassikal <alfredcoffie0@gmail.com>
2022-07-07 16:21:25 -0700

    k

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,45 @@
+#include "main.h"
+
+/**
+ * print_triangle - print triangle with size var
+ * @var: size of a triangle to print
+ * */
+
+void print_triangle(int var)
+{
+	if (var < 1)
+		_putchar('\n');
+	else
+	{
+		int size = 1;
+
+		do {
+			int ii;
+
+			for (ii = 1; ii <= var; ii++)
+			{
+				if (ii > (var - size))
+					_putchar('#');
+				else
+					if (var < 1)
+						_putchar('\n');
+					else
+					{
+						int size = 1;
+
+						do {
+							int ii;
+
+							for (ii = 1; ii <= var; ii++)
+							{
+								if (ii > (var - size))
+									_Putchar('#');
+								else
+									_putchar(' ');
+							}
+							_putchar('\n');
+							size++;
+						} while (size <= var);
+					}
+			}
+
